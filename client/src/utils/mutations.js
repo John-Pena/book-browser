73f1847d64f5da1e76e9a9b2c16472d5746059bc
John-Pena <jmp1495@gmail.com>
2022-07-03 17:39:21 -0500

    set up createUser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,15 @@ export const LOGIN_USER = gql`
 ;`
 
 export const ADD_USER = gql`
-
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const SAVE_BOOK = gql`
