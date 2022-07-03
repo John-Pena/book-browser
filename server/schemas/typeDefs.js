bbfb873694eb9e3f922be40e939cdaac9669728a
John-Pena <jmp1495@gmail.com>
2022-07-03 17:07:56 -0500

    resolvers folder has been set up to replicate user-controller file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,7 +33,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!) Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addBook(title: String!): Book
+    saveBook(title: String!): Book
   }
 `;
 
